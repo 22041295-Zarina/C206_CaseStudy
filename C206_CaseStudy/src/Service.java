c053916434dae6fba4c38c5c1d75153111b1ff87
chewc <chewc@10.175.4.47>
2023-08-15 14:25:58 +0800

    Child class

---
+++ C206_CaseStudy/src/Service.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class Service  {
+
+	private int number;
+	private String name;
+    private String description;
+    public double price;
+
+    public Service(int number, String name, String description, double price) {
+        this.number = number;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    
+    public String toString() {
+        return "\nService Number: " + number + "\nName: " + name + "\nDescription: " + description + "\nPrice: $" + price;
+    }
+
+	public void setName(String newName) {
+		this.name = newName;
+	}
+
+	public void setDescription(String newDescription) {
+		this.description = newDescription;
+	}
+
+	public void setPrice(double newPrice) {
+		this.price = newPrice;
+	}
+
+	public static void addService(ArrayList<Service> serviceList, Service s1) {
+		// TODO Auto-generated method stub
+		
+	}
+}
