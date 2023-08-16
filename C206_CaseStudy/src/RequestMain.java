dddaf67bdeb6cfa29368c917c497d60dc78be882
boble <boble@10.175.5.199>
2023-08-16 11:13:08 +0800

    @request

--- C206_CaseStudy/src/RequestMain.java
+++ C206_CaseStudy/src/RequestMain.java
@@ -7,117 +7,117 @@ import java.util.Scanner;
 
 public class RequestMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		List<Request> requests = new ArrayList<>();
-		 
-		while (true) {
-			System.out.println("1. Add New Request");
-			System.out.println("2. View All Requests");
-			System.out.println("3. Delete Request");
-			System.out.println("4. Exit");
-			System.out.print("Enter your choice: ");
-			int choice = scanner.nextInt();
-			scanner.nextLine(); // Consume the newline
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final List<Request> requests = new ArrayList<>();
 
-			switch (choice) {
-			case 1:
-				System.out.print("Customer ID: ");
-				String custID = scanner.nextLine();
-				String requestDate;
-				do {
-					System.out.print("Enter request date (DD/MM/YYYY): ");
-					requestDate = scanner.nextLine();
-					if (!isValidDateFormat(requestDate)) {
-						System.out.println("Wrong date format. Please follow DD/MM/YYYY format.");
-					}
-				} while (!isValidDateFormat(requestDate));
-				System.out.print("Enter Description: ");
-				String description = scanner.nextLine();
-				String estimatedStartDate;
-				do {
-					System.out.print("Enter esitimated start date date (DD/MM/YYYY): ");
-					estimatedStartDate = scanner.nextLine();
-					if (!isValidDateFormat(estimatedStartDate)) {
-						System.out.println("Wrong date format. Please follow DD/MM/YYYY format.");
-					}
-				} while (!isValidDateFormat(estimatedStartDate));
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("1. Add New Request");
+            System.out.println("2. View All Requests");
+            System.out.println("3. Delete Request");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline
 
-				System.out.print("Enter Budget: ");
-				double budget = scanner.nextDouble();
+            switch (choice) {
+                case 1:
+                    addNewRequest();
+                    break;
+                case 2:
+                    viewAllRequests();
+                    break;
+                case 3:
+                    deleteRequest();
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please select again.");
+            }
+        }
+    }
 
-				requests.add(new Request(custID, requestDate, description, estimatedStartDate, budget));
-				System.out.println("Request added successfully.");
-				break;
-			case 2:
-				if (requests.isEmpty()) {
-					System.out.println("No requests found.");
-				} else {
-					System.out.println("List of Requests:");
-					for (int i = 0; i < requests.size(); i++) {
-						Request request = requests.get(i);
-						System.out.println("Request " + (i + 1));
-						System.out.println("Customer ID: " + request.getCustID());
-						System.out.println("Request Date: " + request.getRequestDate());
-						System.out.println("Description: " + request.getDescription());
-						System.out.println("Estimated Start Date: " + request.getEstimatedStartDate());
-						System.out.println("Budget: " + request.getBudget());
-						System.out.println("----------------------");
-					}
-				}
-				break;
-			// Inside the switch statement, in the "case 3" block
-			case 3:
-				if (requests.isEmpty()) {
-					System.out.println("No requests to delete.");
-				} else {
-					System.out.print("Enter the index of the request to delete: ");
-					int index = scanner.nextInt();
-					scanner.nextLine(); // Consume the newline
+    private static void addNewRequest() {
+        System.out.print("Customer ID: ");
+        String custID = scanner.nextLine();
+        String requestDate;
+        do {
+            System.out.print("Enter request date (DD/MM/YYYY): ");
+            requestDate = scanner.nextLine();
+            if (!isValidDateFormat(requestDate)) {
+                System.out.println("Wrong date format. Please follow DD/MM/YYYY format.");
+            }
+        } while (!isValidDateFormat(requestDate));
+        System.out.print("Enter Description: ");
+        String description = scanner.nextLine();
+        String estimatedStartDate;
+        do {
+            System.out.print("Enter estimated start date (DD/MM/YYYY): ");
+            estimatedStartDate = scanner.nextLine();
+            if (!isValidDateFormat(estimatedStartDate)) {
+                System.out.println("Wrong date format. Please follow DD/MM/YYYY format.");
+            }
+        } while (!isValidDateFormat(estimatedStartDate));
 
-					if (index >= 1 && index <= requests.size()) {
-						System.out.print("Are you sure you want to delete this request? (Y/N): ");
-						String confirmation = scanner.nextLine();
+        System.out.print("Enter Budget: ");
+        double budget = scanner.nextDouble();
 
-						if (confirmation.equalsIgnoreCase("Y")) {
-							requests.remove(index - 1);
-							System.out.println("Request deleted successfully.");
-						} else {
-							System.out.println("Request deletion canceled.");
-						}
-					} else {
-						System.out.println("Invalid request index.");
-					}
+        requests.add(new Request(custID, requestDate, description, estimatedStartDate, budget));
+        System.out.println("Request added successfully.");
+    }
 
-				}
-				break;
+    private static void viewAllRequests() {
+        if (requests.isEmpty()) {
+            System.out.println("No requests found.");
+        } else {
+            System.out.println("List of Requests:");
+            for (int i = 0; i < requests.size(); i++) {
+                Request request = requests.get(i);
+                System.out.println("Request " + (i + 1));
+                System.out.println("Customer ID: " + request.getCustID());
+                System.out.println("Request Date: " + request.getRequestDate());
+                System.out.println("Description: " + request.getDescription());
+                System.out.println("Estimated Start Date: " + request.getEstimatedStartDate());
+                System.out.println("Budget: " + request.getBudget());
+                System.out.println("----------------------");
+            }
+        }
+    }
 
-			case 4:
-				System.out.println("Exiting...");
-				System.exit(0);
-			default:
-				System.out.println("Invalid choice. Please select again.");
-			}
-		}
-	}
+    private static void deleteRequest() {
+        if (requests.isEmpty()) {
+            System.out.println("No requests to delete.");
+        } else {
+            System.out.print("Enter the index of the request to delete: ");
+            int index = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline
 
-	private static boolean isValidDateFormat(String date) {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		sdf.setLenient(false);
-		try {
-			Date parsedDate = sdf.parse(date);
-			return true;
-		} catch (ParseException e) {
-			return false;
-		}
-	}
+            if (index >= 1 && index <= requests.size()) {
+                System.out.print("Are you sure you want to delete this request? (Y/N): ");
+                String confirmation = scanner.nextLine();
 
-	public static List<Request> getRequests() {
-		
-		// TODO Auto-generated method stub
-		return null;
-		
-		
-	}
+                if (confirmation.equalsIgnoreCase("Y")) {
+                    requests.remove(index - 1);
+                    System.out.println("Request deleted successfully.");
+                } else {
+                    System.out.println("Request deletion canceled.");
+                }
+            } else {
+                System.out.println("Invalid request index.");
+            }
+        }
+    }
+
+    private static boolean isValidDateFormat(String date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setLenient(false);
+        try {
+            Date parsedDate = sdf.parse(date);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
 }
